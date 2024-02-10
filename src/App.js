60848a87631a5d09d081715eef0a3f2c5c560bab
Piyushdigraskar <piyushdigraskar3@gmail.com>
2024-02-10 11:04:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from "react";
+import Header from "./Components/Layout/Header";
+
+
+function App() {
+  return (
+    <Fragment>
+      <Header></Header>
+    </Fragment>
+  );
+}
+
+export default App;
